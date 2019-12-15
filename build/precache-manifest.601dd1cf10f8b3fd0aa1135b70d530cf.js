7eb2f61afcdb4fcc7ef79db135a819d71d1cd56f
LukasGrinys <lukas.grinys@gmail.com>
2019-12-15 12:44:57 +0200

    Adding build folder

---
+++ build/precache-manifest.601dd1cf10f8b3fd0aa1135b70d530cf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15d28e52753d1a92a710bac197a702e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1efe9ec5259e3f2d074",
+    "url": "/static/css/main.7bb561d3.chunk.css"
+  },
+  {
+    "revision": "a2893dfcca5f790d126c",
+    "url": "/static/js/2.396ca0b0.chunk.js"
+  },
+  {
+    "revision": "b1efe9ec5259e3f2d074",
+    "url": "/static/js/main.81acefc3.chunk.js"
+  },
+  {
+    "revision": "118296307d0bbc188093",
+    "url": "/static/js/runtime-main.52d91cfd.js"
+  },
+  {
+    "revision": "29c920f8447ecf51986955118909e3bc",
+    "url": "/static/media/barsButton.29c920f8.svg"
+  },
+  {
+    "revision": "464128e7e5f72a712b7c752996e86b58",
+    "url": "/static/media/closeButton.464128e7.svg"
+  },
+  {
+    "revision": "107cb93332036fa1991577a4f1a10cd6",
+    "url": "/static/media/connection.107cb933.svg"
+  },
+  {
+    "revision": "64fb8a6c031e021a72de8ea970588908",
+    "url": "/static/media/contact.64fb8a6c.svg"
+  },
+  {
+    "revision": "e9398db995f4171a5b469d9e817f60c3",
+    "url": "/static/media/contactHeader.e9398db9.jpeg"
+  },
+  {
+    "revision": "22e66e3f99082c52e582f4ee72d76ff9",
+    "url": "/static/media/header.22e66e3f.jpg"
+  },
+  {
+    "revision": "9aa3135acea46c7d7964e89ee8dd1347",
+    "url": "/static/media/options.9aa3135a.svg"
+  },
+  {
+    "revision": "f6397f4e1365350d7fa9b6da5488eb55",
+    "url": "/static/media/partnersHeader.f6397f4e.jpeg"
+  },
+  {
+    "revision": "a7f85961b1a51629b00e08b4ed188164",
+    "url": "/static/media/productsHeader.a7f85961.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/service.c1a95f73.svg"
+  }
+]);
